3163023a4e095f09583301c9ac8a9bcc457efa22
Nazmi <nazmiyazkan@gmail.com>
2024-05-12 01:08:08 +0300

    assistan page design

--- src/pages/Assistant.jsx
+++ src/pages/Assistant.jsx
@@ -1,7 +1,27 @@
+import "./Assistant.scss";
+
 function Assistant() {
   return (
     <>
-      <div>Asistan sayfası</div>
+      <div className="assistant">
+        <div className="section">
+          <h1>Asistan Anasayfası</h1>
+          <div>
+            <label htmlFor="">Asistan Adı: </label>
+            <label className="infoLabel">Nazmi Yazkan</label>
+          </div>
+
+          <div>
+            <label htmlFor="">Sicil No: </label>
+            <label className="infoLabel">2313123</label>
+          </div>
+          <div className="btnGroup">
+            <button type="submit">Müşteri Çağrı Listesi Menüsü</button>
+            <button type="submit">Aylık Prim Listesi Menüsü</button>
+            <button type="submit">Prim İtiraz Listesi Menüsü</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
